3f361183c885ebc2980ebcef295c0cbcd29ab4c6
Willt617 <85527300+Willt617@users.noreply.github.com>
2021-09-13 19:54:08 -0400

    Everything but win condition finished

---
+++ scripts.js
@@ -0,0 +1,45 @@
+function init(){
+window.confirm(" Lets play Tik Tak Toe.");
+}
+
+let board = [
+    '','','','','','','','',''
+];
+let Win = [
+    
+]
+let player1;
+let player2;
+let turn;
+
+function setPlayers(){
+    let playerText = document.getElementById("players");
+    player1 = document.getElementById("player1").value;
+    player2 = document.getElementById("player2").value;
+    turn = player1
+    console.log("setPlayers was called");
+    playerText.innerHTML = `Player 1: ${player1} is X <br> Player 2: ${player2} is O`;
+}
+
+function markXO(id){
+    let spot = document.getElementById(id);
+    let turns = document.getElementById("turn");
+    let mark = whosTurn();
+    turns.innerHTML = `It is ${turn}'s turn '`
+    spot.innerText = mark;
+    board[id] = mark;
+}
+function whosTurn(){   
+    if(turn == player1){
+       turn = player2;
+       return "X";   
+    }
+    if(turn == player2){
+       turn = player1;
+       return "O";
+    }
+}
+function newGame(){
+
+}
+window.addEventListener("load", init);
